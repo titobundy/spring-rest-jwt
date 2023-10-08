1436ed4dd9095c14ca1acff6c7b3f73e0b74a8a1
Hector Diaz <hediazd@falabella.cl>
2023-10-08 18:34:50 -0300

    spring rest using spring jpa and h2

--- src/main/java/cl/personal/jpa/repository/UserRepository.java
+++ src/main/java/cl/personal/jpa/repository/UserRepository.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import cl.personal.jpa.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends CrudRepository<User, Long> {
 
-	List<User> findByLastName(String lastName);
+	List<User> findByLastName(@Param("name") String lastName);
 
 	User findById(long id);
 }
