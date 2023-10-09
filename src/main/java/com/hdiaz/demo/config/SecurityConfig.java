283a93ee86b8f735358143b57dd7ed25d6890448
Hector Diaz <hediazd@falabella.cl>
2023-10-09 12:34:36 -0300

    spring security with basic auth

--- src/main/java/com/hdiaz/demo/config/SecurityConfig.java
+++ src/main/java/com/hdiaz/demo/config/SecurityConfig.java
@@ -1,8 +1,21 @@
-//package com.hdiaz.demo.config;
-//
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//}
+package com.hdiaz.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
